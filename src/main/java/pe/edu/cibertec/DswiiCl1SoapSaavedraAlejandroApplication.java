fc13a8d4a97e3000bae7f1855f0a72a27f47e18d
ajSL17 <zero_gmz@outlook.com>
2024-06-08 23:27:10 -0500

    Commit

---
+++ src/main/java/pe/edu/cibertec/DswiiCl1SoapSaavedraAlejandroApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapSaavedraAlejandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapSaavedraAlejandroApplication.class, args);
+	}
+
+}
